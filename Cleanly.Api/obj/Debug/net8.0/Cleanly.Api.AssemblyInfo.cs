bdf2531028ff25e578d958d0fe4b0a6576f15049
Luiz Miranda <ljunior@abinfo.pt>
2026-04-12 16:30:41 +0100

    merge

--- Cleanly.Api/obj/Debug/net8.0/Cleanly.Api.AssemblyInfo.cs
+++ Cleanly.Api/obj/Debug/net8.0/Cleanly.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cleanly.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8abf6879cfd794fc380db2912e037e5778d7187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34fa459655289ab079409b8a0a94e3c5057b18cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cleanly.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cleanly.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
